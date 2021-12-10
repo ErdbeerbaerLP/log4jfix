2ce3f33f36dfb7400fa53561c8f7957e6ba6fd8c
ErdbeerbaerLP <admin@erdbeerbaerlp.de>
2021-12-10 04:35:42 +0100

    1.12 Port

--- src/main/java/de/erdbeerbaerlp/log4jfix/Log4jfix.java
+++ src/main/java/de/erdbeerbaerlp/log4jfix/Log4jfix.java
@@ -2,37 +2,38 @@ package de.erdbeerbaerlp.log4jfix;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ServerChatEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("log4jfix")
+
+@Mod( modid = Log4jfix.MOD_ID,name = Log4jfix.MOD_NAME,version = Log4jfix.VERSION, serverSideOnly = true, acceptableRemoteVersions = "*"
+)
 public class Log4jfix {
 
+    public static final String MOD_ID = "log4jfix";
+    public static final String MOD_NAME = "Log4J RCE Fix";
+    public static final String VERSION = "1.0.0";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     private final Pattern p = Pattern.compile("jndi:ldap");
 
-    public Log4jfix() {
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (s, b) -> true));
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    public void chat(final ServerChatEvent event) {
+    public void chat(ServerChatEvent event) {
         final Matcher m = this.p.matcher(event.getMessage());
         if (m.find()) {
-            LOGGER.warn(event.getPlayer().getName().getFormattedText() + " just attempted to do something malicious!");
+            LOGGER.warn(event.getPlayer().getName() + " just attempted to do something malicious!");
             event.setCanceled(true);
         }
     }
